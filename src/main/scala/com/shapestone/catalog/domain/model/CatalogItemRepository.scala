8e0490729c810e415d524e11e31c5c613302c250
Michael Sundell <msundell@gmail.com>
2021-08-25 22:03:32 -0700

    Initial commit

--- src/main/scala/com/shapestone/catalog/domain/model/CatalogItemRepository.scala
+++ src/main/scala/com/shapestone/catalog/domain/model/CatalogItemRepository.scala
@@ -2,4 +2,13 @@ package com.shapestone.catalog.domain.model
 
 trait CatalogItemRepository {
 
+  def addCatalogItem(catalogId: String, item: CatalogItem): Unit
+
+  def getCatalogItem(catalogId: String, item: CatalogItem): CatalogItem
+
+  def updateCatalogItem(catalogId: String, itemId: String): Unit
+
+  def removeCatalogItem(catalogId: String, itemId: String): Unit
+
+  def nextCatalogItemId: String;
 }
--- src/main/scala/com/shapestone/catalog/domain/model/CatalogRepository.scala
+++ src/main/scala/com/shapestone/catalog/domain/model/CatalogRepository.scala
@@ -2,4 +2,13 @@ package com.shapestone.catalog.domain.model
 
 trait CatalogRepository {
 
+  def createCatalog(catalog: Catalog): Unit
+
+  def getCatalog: Catalog
+
+  def updateCatalog(catalog: Catalog): Unit
+
+  def removeCatalog(catalogId: String): Unit
+
+  def nextCatalogId: String;
 }
